0ed75c44a684485401ead96284d5ebe4bbfcb0ce
Cuhassle <143834475+cuhassle@users.noreply.github.com>
2025-10-16 20:43:12 -0700

    Changes (very non specific lol)

--- games/games.js
+++ games/games.js
@@ -1,19 +1,17 @@
 class game {
-    constructor(name, url, width,height) {
+    constructor(name, url) {
         this.name = name;
         this.url = url;
-        this.width = width;
-        this.height = height;
     }
 }
 
 var games = [
-    new game("Totally Accurate Battle Simulator", "./totallyaccuratearmorsim/", 960, 600),
+    new game("Totally Accurate Battle Simulator", "./totallyaccuratearmorsim/"),
     new game("Parkour02", "parkour02")
 ]
 
 function changegame(gametogoto) {
     var gamecontainer = document.getElementById("gamecontainer");
     gamecontainer.src = gametogoto.url;
-    console.log("hi")
+    document.getElementById("gamediv").style.display = "block";
 }
