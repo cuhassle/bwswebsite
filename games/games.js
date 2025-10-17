787b78e246e5acdb7e19b803d72fc01c86a7a2d5
Cuhassle <143834475+cuhassle@users.noreply.github.com>
2025-10-16 21:19:47 -0700

    Games update

--- games/games.js
+++ games/games.js
@@ -10,8 +10,28 @@ var games = [
     new game("Parkour02", "parkour02")
 ]
 
+document.addEventListener("DOMContentLoaded", populategamecatalog);
+
 function changegame(gametogoto) {
     var gamecontainer = document.getElementById("gamecontainer");
     gamecontainer.src = gametogoto.url;
     document.getElementById("gamediv").style.display = "block";
+}
+
+function populategamecatalog() {
+    var container = document.getElementById("catalog")
+    for (var i = 0; i < games.length; i += 1) {
+        var entry = document.createElement("div");
+        const gamenumber = i;
+        entry.classList = "catalog-item water"
+        entry.onclick = function () { changegame(games[gamenumber]) };
+        entry.style = "cursor:pointer;"
+        
+        var insideelement = document.createElement("p");
+        insideelement.innerText = games[i].name;
+        entry.appendChild(insideelement);
+
+        container.appendChild(entry);
+    }
+    console.log("Done")
 }
