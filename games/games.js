810fdaf29def7003603c39e1abea64e165cac0de
Cuhassle <143834475+cuhassle@users.noreply.github.com>
2025-10-17 20:53:47 -0700

    Icons and stuff

--- games/games.js
+++ games/games.js
@@ -1,13 +1,24 @@
 class game {
-    constructor(name, url) {
+    constructor(name, url, icon, description) {
         this.name = name;
         this.url = url;
+        this.icon = icon;
+        this.description = description;
     }
 }
 
 var games = [
-    new game("Totally Accurate Battle Simulator", "./totallyaccuratearmorsim/"),
-    new game("Parkour02", "parkour02")
+    new game(
+        "Totally Accurate Battle Simulator", 
+        "./totallyaccuratearmorsim/", 
+        "./totallyaccuratearmorsim/LogoTAAS.png",
+        "It's a military simulator with lots of units, lots of levels, and lots of chaos."
+    ),
+    new game(
+        "Parkour02", 
+        "./parkour02", 
+        "./parkour02/Parkor02Logo.png", 
+        "TESTSETESTESTESTESTESETESTESTSETE STSTESTSETESTESTESTESTESETEST ESTSETESTSTESTSETESTESTE STESTESETESTESTSETESTSsssss/parkour02/Parkor02Logo.png/parkour02/Parkor02Logo.png")
 ]
 
 document.addEventListener("DOMContentLoaded", populategamecatalog);
@@ -27,10 +38,17 @@ function populategamecatalog() {
         entry.onclick = function () { changegame(games[gamenumber]) };
         entry.style = "cursor:pointer;"
         
-        var insideelement = document.createElement("p");
-        insideelement.innerText = games[i].name;
-        entry.appendChild(insideelement);
+        if (games[i].icon) {
+            var icon = document.createElement("img");
+            icon.src = games[i].icon;
+            icon.style.width = "25%"
+            entry.appendChild(icon);
+        }
 
+        var gameinfo = document.createElement("div");
+        gameinfo.classList = "game-entryinfo";
+        gameinfo.innerHTML = `<h2>${games[i].name}</h2><p style="word-break: keep-all;">${games[i].description}</p>`;
+        entry.appendChild(gameinfo);
         container.appendChild(entry);
     }
     console.log("Done")
